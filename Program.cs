91190993db19db9daa4f3b3593ca76124f4d38f2
thanhdat-06 <99951342+thanhdat-06@users.noreply.github.com>
2022-03-18 17:08:45 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using testmvcvssql.Services;
+using Microsoft.EntityFrameworkCore;
+using testmvcvssql.Database;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("myconnect")));
+
+builder.Services.AddScoped<CategoryService>();
+
+var app = builder.Build();
+
+
+
+// builder.Services.AddSingleton<CategoryService>();
+// builder.Services.AddTransient
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // app.UseHsts();
+}
+
+// app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+//dotnet ef migrations add <AddTableCategoryVSProduct>
